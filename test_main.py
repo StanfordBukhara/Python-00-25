77fcc89926ec100cd40d64bcb32a114c57ca8894
AsdSob <34741911+AsdSob@users.noreply.github.com>
2025-06-13 03:24:26 +0500

    Delete test_main.py

--- test_main.py
+++
@@ -1,7 +0,0 @@
-from main import count_characters
-
-def test_basic():
-    assert count_characters("Hello") == 5
-    assert count_characters("") == 0
-    assert count_characters("Python 3.11") == 11
-    assert count_characters("😀😃😄") == 3
